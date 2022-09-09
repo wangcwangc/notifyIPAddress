e672cfcd19ee7a3d8a3434b807e7d80b74300019
wangc <858256500@qq.com>
2022-09-09 19:00:49 +0800

    read config file

--- ipv6.py
+++ ipv6.py
@@ -1,6 +1,8 @@
 # author : wangcwangc
 # time : 2022/9/8 14:27
 import netifaces
+import time
+import configparser
 
 
 def get_host_ipv6():
@@ -14,5 +16,28 @@ def get_host_ipv6():
         return ipv6
 
 
-if __name__ == '__main__':
-    print(get_host_ipv6())
+def loop_monitor():
+    while True:
+        get_host_ipv6()
+        time.sleep(60)  # 每分钟运行一次
+
+
+def read_config():
+    file = 'config.ini'
+    config = configparser.ConfigParser()
+    config.read(file)
+    webhook = config.get('url', 'webhook')
+    print(webhook)
+    if webhook == '':
+        print('请配置webhook地址')
+        exit(0)
+    ipv6 = config.get('ip', 'ipv6')
+    if ipv6 == '':
+        ipv6 = get_host_ipv6()
+        print(ipv6)
+        config.set('ip', 'ipv6', ipv6)
+        config.write(open('config.ini', 'w'))
+
+
+if __name__ == "__main__":
+    read_config()
